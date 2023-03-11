104aeab5ad8f23db73bf0216280450d91e601084
pbrunet <brunetpierrick@gmail.com>
2023-03-11 22:10:07 +0100

    Spawn level1 exit as far as possible from the player

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
 public class LevelGenerator : MonoBehaviour
 {
 
     [SerializeField] int NumCells = 10;
+    int RemainingNumCells;
     [SerializeField] string nextLevel = "";
     [SerializeField] GoToLevel nextLevelPrefab;
 
@@ -19,6 +23,7 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        RemainingNumCells = NumCells;
         grid = new GeneratedCell[NumCells][];
         for (int i = 0; i < NumCells; i++)
         {
@@ -27,30 +32,27 @@ public class LevelGenerator : MonoBehaviour
 
         List<Tuple<int, int>> GeneratedPos = new List<Tuple<int, int>>();
 
-        GenNeighboor(GetComponent<GeneratedCell>(), new Tuple<int, int>(5, 5), GeneratedPos);
+        GenNeighboor(GetComponent<GeneratedCell>(), new Tuple<int, int>(NumCells / 2, NumCells / 2), GeneratedPos);
 
 
         for (int i = 0; i < grid.Length; i++)
         {
             for (int j = 0; j < grid[i].Length; j++)
             {
-                if (i == 5 && j == 5) continue; // This is the seed!
+                if (i == NumCells / 2 && j == NumCells / 2) continue; // This is the seed!
                 if (grid[i][j] == null) continue;
-                Instantiate(grid[i][j], new Vector3((i - 5) * xSize, (j - 5) * ySize, gameObject.transform.position.z), gameObject.transform.rotation, gameObject.transform);
+                Instantiate(grid[i][j], new Vector3((i - NumCells / 2) * xSize, (j - NumCells / 2) * ySize, gameObject.transform.position.z), gameObject.transform.rotation, gameObject.transform);
             }
         }
 
-        Tuple<int, int> cellWithExit = GeneratedPos[UnityEngine.Random.Range(0, GeneratedPos.Count)];
-        GeneratedCell genWithExit = grid[cellWithExit.Item1][cellWithExit.Item2];
-        GoToLevel spawnedNextLevel = Instantiate<GoToLevel>(nextLevelPrefab, new Vector3((cellWithExit.Item1 - 5) * xSize + genWithExit.possibleExitPos.x + nextLevelPrefab.transform.position.x, (cellWithExit.Item2 - 5) * ySize + genWithExit.possibleExitPos.y + nextLevelPrefab.transform.position.y, gameObject.transform.position.z), gameObject.transform.rotation, gameObject.transform);
-        spawnedNextLevel.SetNextLevel(nextLevel);
+        SpawnNextLevel();
     }
 
     bool GenNeighboor(GeneratedCell currentCell, Tuple<int, int> pos, List<Tuple<int, int>> generatedPos)
     {
         Debug.Log("Generate " + currentCell.name + " at pos " + pos.Item1.ToString() + "/" + pos.Item2.ToString() + "\n");
         grid[pos.Item1][pos.Item2] = currentCell;
-        NumCells--;
+        RemainingNumCells--;
 
         List<Tuple<int, int>> localGeneratedPos = new List<Tuple<int, int>>();
         localGeneratedPos.Add(pos);
@@ -92,7 +94,7 @@ public class LevelGenerator : MonoBehaviour
                 break;
             }
 
-            if (NumCells == 0)
+            if (RemainingNumCells == 0)
             {
                 Debug.Log("Stop generating cells, numCells reached\n");
                 failed = true;
@@ -136,7 +138,7 @@ public class LevelGenerator : MonoBehaviour
             foreach (Tuple<int, int> newPos in localGeneratedPos)
             {
                 grid[newPos.Item1][newPos.Item2] = null;
-                NumCells++;
+                RemainingNumCells++;
             }
             return false;
         }
@@ -167,7 +169,7 @@ public class LevelGenerator : MonoBehaviour
 
     GeneratedCell PickCell(Tuple<int, int> newPos, List<GeneratedCell> possibleCells)
     {
-        HashSet<GeneratedCell> cells = new HashSet<GeneratedCell>( possibleCells );
+        HashSet<GeneratedCell> cells = new HashSet<GeneratedCell>(possibleCells);
 
         for (int i = 0; i < 4; i++)
         {
@@ -214,9 +216,84 @@ public class LevelGenerator : MonoBehaviour
         return cells.ElementAt(UnityEngine.Random.Range(0, cells.Count));
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnNextLevel()
     {
+        int[][] distFromSeed = new int[NumCells][];
+        for (int i = 0; i < NumCells; i++)
+        {
+            distFromSeed[i] = new int[NumCells];
+            Array.Fill<int>(distFromSeed[i], NumCells * NumCells);
+        }
+
+        List<Tuple<int, int>> Candidate = new List<Tuple<int, int>>();
+        HashSet<Tuple<int, int>> ToProcess = new HashSet<Tuple<int, int>>();
+        HashSet<Tuple<int, int>> ToProcessNextDist = new HashSet<Tuple<int, int>>();
+
+        int currentDist = 0;
+        ToProcess.Add(new Tuple<int, int>(NumCells / 2, NumCells / 2));
+        distFromSeed[NumCells / 2][NumCells / 2] = currentDist;
+        currentDist++;
+
+        do
+        {
+            Candidate.Clear();
+            while (ToProcess.Count > 0)
+            {
+                Tuple<int, int> pos = ToProcess.Last();
+                ToProcess.Remove(pos);
+                Candidate.Add(pos);
+
+                for (int i = 0; i < 4; i++)
+                {
+
+                    Tuple<int, int> newPos;
+                    switch ((GeneratedCell.NeightborSide)i)
+                    {
+                        case GeneratedCell.NeightborSide.Left:
+                            newPos = new Tuple<int, int>(pos.Item1 - 1, pos.Item2); break;
+                        case GeneratedCell.NeightborSide.Right:
+                            newPos = new Tuple<int, int>(pos.Item1 + 1, pos.Item2); break;
+                        case GeneratedCell.NeightborSide.Top:
+                            newPos = new Tuple<int, int>(pos.Item1, pos.Item2 + 1); break;
+                        case GeneratedCell.NeightborSide.Bottom:
+                            newPos = new Tuple<int, int>(pos.Item1, pos.Item2 - 1); break;
+                        default:
+                            newPos = new Tuple<int, int>(pos.Item1, pos.Item2 - 1);
+                            break;
+                    }
+
+                    if (newPos.Item1 < 0 || newPos.Item2 < 0 || newPos.Item1 >= grid.Length || newPos.Item2 >= grid[0].Length || grid[newPos.Item1][newPos.Item2] == null)
+                    {
+                        continue;
+                    }
+
+                    if(!grid[pos.Item1][pos.Item2].neighborCell[i].cells.Contains(grid[newPos.Item1][newPos.Item2]))
+                    {
+                        // No path from this cell to the other one
+                        continue;
+                    }
+
+                    if (distFromSeed[newPos.Item1][newPos.Item2] <= currentDist)
+                    {
+                        // This cell is already processed or empty
+                        continue;
+                    }
+
+                    distFromSeed[newPos.Item1][newPos.Item2] = currentDist;
+                    ToProcessNextDist.Add(newPos);
+                }
+            }
+
+            ToProcess = ToProcessNextDist;
+            ToProcessNextDist = new HashSet<Tuple<int, int>>();
+            currentDist++;
+        } while (ToProcess.Count > 0);
+
+
+        Tuple<int, int> cellWithExit = Candidate[UnityEngine.Random.Range(0, Candidate.Count)];
+        GeneratedCell genWithExit = grid[cellWithExit.Item1][cellWithExit.Item2];
+        GoToLevel spawnedNextLevel = Instantiate<GoToLevel>(nextLevelPrefab, new Vector3((cellWithExit.Item1 - NumCells / 2) * xSize + genWithExit.possibleExitPos.x + nextLevelPrefab.transform.position.x, (cellWithExit.Item2 - NumCells / 2) * ySize + genWithExit.possibleExitPos.y + nextLevelPrefab.transform.position.y, gameObject.transform.position.z), gameObject.transform.rotation, gameObject.transform);
+        spawnedNextLevel.SetNextLevel(nextLevel);
 
     }
 }
